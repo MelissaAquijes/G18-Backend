ad118eb645ff59bc6af414db041ed56d55f79fb7
Efrain Quispe <EfrainQuispe156@gmail.com>
2024-07-24 22:41:56 -0500

    CRUD + claudinary

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CategoriaModel, ProductoModel
 
-# Register your models here.
+@admin.register(CategoriaModel)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+@admin.register(ProductoModel)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion', 'precio', 'categoria')
+    search_fields = ('nombre', 'categoria__nombre')
+    list_filter = ('categoria',)
