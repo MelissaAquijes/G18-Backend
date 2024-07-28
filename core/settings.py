0a8f3fc3852e89aa458918acbbc8ccf493d360a0
Efrain Quispe <EfrainQuispe156@gmail.com>
2024-07-28 00:05:52 -0500

    before deploitment

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-}
+    'CLOUD_NAME' : config("CLOUD_NAME") ,
+    'API_KEY' :config("API_KEY"), 
+    'API_SECRET':config("API_SECRET") 
+    }
 
 CORS_ALLOW_ALL_ORIGINS = True
 
