eb5e273d8cda6b3e42de2a7983895302d168a3ca
Melissa Aquijes <melissa.geral10@gmail.com>
2024-07-27 21:41:12 -0500

    Personalizacion de la tabla users en el admin.

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     'rest_framework',
-    "corsheaders",
+    'corsheaders',
+    'rest_framework_simplejwt',
     'products',
     'users',
     'jazzmin'
@@ -130,3 +131,16 @@ CLOUDINARY_STORAGE = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
+}
